0fb52e5da892b4498b6edd94edc27663d26c8fe8
Pranava-Kumar <150614649+Pranava-Kumar@users.noreply.github.com>
2025-08-09 00:50:10 +0530

    Delete run_test.py

--- run_test.py
+++
@@ -1,7 +0,0 @@
-from quantum_starter_lab.api import make_bell
-
-print("--- Running quantum-starter-lab! ---")
-results = make_bell(backend="qiskit.aer", seed=42)
-print(results)
-results.plot()
-print("--- Demo complete! ---")
